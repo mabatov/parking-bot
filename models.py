f868d5783995743c1d5b1e9e3638aaf2c05c2b3d
nmabatov <nikita-mabatov@mail.ru>
2025-01-06 23:51:23 +0300

    models.py upd

--- models.py
+++ models.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True, nullable=False)
+    __tablename__ = "users"
 
+    id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, unique=True, index=True)
