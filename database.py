1ec23e5a55cd956921ca386a24a0554e47f6d58c
nmabatov <nikita-mabatov@mail.ru>
2025-01-07 00:50:33 +0300

    skip admin adding to db

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from models import Base, User
 import os
 
+ADMIN_TELEGRAM_ID = int(os.getenv("ADMIN_ID"))
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
@@ -12,6 +13,9 @@ def init_db():
     Base.metadata.create_all(bind=engine)
 
 def check_user_access(user_id: int) -> bool:
+    # Пропускаем проверку для администратора
+    if user_id == ADMIN_TELEGRAM_ID:
+        return True
     session = SessionLocal()
     user = session.query(User).filter(User.telegram_id == user_id).first()
     session.close()
