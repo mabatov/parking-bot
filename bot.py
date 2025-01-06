0736473d44494c57841ebe6f08d0a64880fbd8f1
nmabatov <nikita-mabatov@mail.ru>
2025-01-07 01:11:12 +0300

    skip admin adding to db

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text("У вас нет доступа к этому боту.")
 
 
-async def get_photo_from_rtsp(update: Update, context) -> None:
+async def get_photo_from_rtsp(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     cap = cv2.VideoCapture(RTSP_URL)
     ret, frame = cap.read()
     if ret:
